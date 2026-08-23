cdf2e8da6bd499d3454d457a1a556f40f944fcb7
Mihail <48523152+SexyTolik@users.noreply.github.com>
2026-08-23 10:02:03 +0500

    Add new font & JournalAssets

---
+++ Assets/Progress/Scripts/JournalData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "JournalData", menuName = "Scriptable Objects/JournalData")]
+public class JournalData : ScriptableObject
+{
+    public int JournalID = -1;
+    
+}
