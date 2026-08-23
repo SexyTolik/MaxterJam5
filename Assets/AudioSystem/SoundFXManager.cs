dec80d4a74fcd6ebcd9c3562313a7a1e8f846dc2
Mihail <48523152+SexyTolik@users.noreply.github.com>
2026-08-23 17:48:30 +0500

    SimpleAudioManager

---
+++ Assets/AudioSystem/SoundFXManager.cs
@@ -0,0 +1,46 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class SoundFXManager : MonoBehaviour
+{
+   public static SoundFXManager instance;
+   [SerializeField] private AudioSource soundFXPrefab;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void PlayAudioClip(AudioClip audioClip, Transform position)
+    {
+        AudioSource source = Instantiate(soundFXPrefab,position.position,quaternion.identity);
+        source.clip = audioClip;
+        source.Play();
+        float clipLenght = audioClip.length;
+        Destroy(source,clipLenght);
+    }
+    public void PlayAudioClip(AudioClip audioClip, Vector3 position)
+    {
+        AudioSource source = Instantiate(soundFXPrefab,position,quaternion.identity);
+        source.clip = audioClip;
+        source.Play();
+        float clipLenght = audioClip.length;
+        Destroy(source,clipLenght);
+    }
+    public void PlayAudioClip(AudioClip audioClip)
+    {
+        AudioSource source = Instantiate(soundFXPrefab,Camera.main.transform.position,quaternion.identity);
+        source.clip = audioClip;
+        source.Play();
+        float clipLenght = audioClip.length;
+        Destroy(source,clipLenght);
+    }
+}
