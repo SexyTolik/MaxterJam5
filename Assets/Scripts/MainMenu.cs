4ceee34b79f60714ca63e38a5b312d561c70fbf5
BagStrider <72280866+BagStrider@users.noreply.github.com>
2026-08-24 12:19:16 +0300

    Added Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button _playButton;
+    [SerializeField] private Button _quitButton;
+
+    private void OnEnable()
+    {
+        _playButton.onClick.AddListener(StartGame);
+        _quitButton.onClick.AddListener(QuitGame);
+        
+    }
+
+    private void OnDisable()
+    {
+        _playButton.onClick.RemoveListener(StartGame);
+        _quitButton.onClick.RemoveListener(QuitGame);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("Room");
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+}
